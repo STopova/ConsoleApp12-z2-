11f8778d663c4b1f854669520b91315c49917cdd
Sevim Topova <sevimtopova@abv.bg>
2024-12-02 14:23:38 +0200

    Add project files.

---
+++ ConsoleApp12(z2)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Second_Task
+{
+    class FibonacciEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int sumEven = 0;
+
+            while (a <= 4000000)
+            {
+                if (a % 2 == 0)
+                {
+                    sumEven += a;
+                }
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+
+            Console.WriteLine(sumEven);
+        }
+    }
+}
